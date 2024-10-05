48da95e85f242bafd15190bf9d194fc2941d64f6
Nasser <nasser@pop-os.home>
2024-10-05 06:34:10 +0200

    feat: with use callback

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,28 @@
-import { useState,useMemo } from "react";
+import { useState, useCallback } from "react";
+import TaskList from "./components/TaskList";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  const [taskList, setTaskList] = useState([]);
-  const costlyResult = useMemo(()=>costlyComputation(counter),[counter]);
+  const [taskCount, setTaskCount] = useState(0);
+  const [taskArray, setTaskArray] = useState([]);
 
-  const incrementCounter = () => {
-    setCounter((prevCount) => prevCount + 1);
+  const increaseCount = () => {
+    setTaskCount((prevCount) => prevCount + 1);
   };
 
-  const addNewTask = () => {
-    setTaskList((prevTasks) => [...prevTasks, "New Task"]);
-  };
+  const addTask = useCallback(() => {
+    setTaskArray((prevTasks) => [...prevTasks, "New Task"]);
+  }, [taskArray]);
 
   return (
-    <div>
-      <div>
-        <h2>Task List</h2>
-        {taskList.map((task, index) => (
-          <p key={index}>{task}</p>
-        ))}
-        <button onClick={addNewTask}>Add Task</button>
-      </div>
+    <>
+      <TaskList tasks={taskArray} addTask={addTask} />
       <hr />
       <div>
-        Count: {counter}
-        <button onClick={incrementCounter}>+</button>
-        <h2>Costly Computation Result</h2>
-        {costlyResult}
+        Count: {taskCount}
+        <button onClick={increaseCount}>+</button>
       </div>
-    </div>
+    </>
   );
-};
-
-const costlyComputation = (value) => {
-  console.log("Performing costly computation...");
-  console.time("Costly Computation Time");
-  for (let i = 0; i < 1000000000; i++) {
-    value += 1;
-  }
-  console.timeEnd("Costly Computation Time");
-  return value;
 };
 
 export default App;
